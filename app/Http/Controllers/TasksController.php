95a22c5864196be365062e025c665f22977d0fb2
Amelie <amelie.a.la.campagne@gmail.com>
2017-08-07 07:24:11 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,6 +47,10 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'status' => 'required|max:',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
         $task->save();
@@ -93,6 +97,10 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+        ]);
+        
         $task = Task::find($id);
         $task->content = $request->content;
         $task->save();
