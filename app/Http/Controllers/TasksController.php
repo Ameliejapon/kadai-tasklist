be60ad66f3dd38d78b3ffd7baeb6090ff9181733
Amelie <amelie.a.la.campagne@gmail.com>
2017-08-07 07:38:38 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -48,10 +48,12 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'status' => 'required|max:',
+            'status' => 'required|max:10', 
+            'content' => 'required|max:255',
         ]);
         
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -99,9 +101,11 @@ class TasksController extends Controller
     {
         $this->validate($request, [
             'status' => 'required|max:10',
+            'content' => 'required|max:255',
         ]);
         
         $task = Task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
