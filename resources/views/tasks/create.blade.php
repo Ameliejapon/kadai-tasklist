6722a83dcba57ab9d4372bbd4d26d7c1f7d518a2
Amelie <amelie.a.la.campagne@gmail.com>
2017-08-09 14:30:54 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,17 +3,26 @@
 @section('content')
 
     <h1>タスク新規作成ページ</h1>
+    <div class="row">
+        <div class="col-xs-12"></div>
+        <div class="col-sm-offset-2 col-sm-8"></div>
+        <div class="col-md-offset-2 col-md-8"></div>
+        <div class="col-lg-offset-3 col-lg-6"></div>
+        {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-    
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-    
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+            <div class="form-group">
+                {!! Form::label('status', 'ステータス:') !!}
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}
+            </div>
         
-        {!! Form::submit('投稿') !!}
+            <div class="form-group">
+                {!! Form::label('content', 'タスク:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
         
-    {!! Form::close() !!}
+            {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+        {!! Form::close() !!}
+    </div>
 
 @endsection
