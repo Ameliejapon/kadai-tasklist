bf62f236846ea795cf2d9677b0812fcf571175d4
Amelie <amelie.a.la.campagne@gmail.com>
2017-08-10 12:32:56 +0000

    form2

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,11 +5,7 @@
     <h1>タスク新規作成ページ</h1>
     <div class="row">
         <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
-            <form>  
-            </form>  
-        </div>
-        
-        {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
             <div class="form-group">
                 {!! Form::label('status', 'ステータス:') !!}
@@ -23,7 +19,9 @@
         
             {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
         
-        {!! Form::close() !!}
+            {!! Form::close() !!}
+        </div>
+        
     </div>
 
 @endsection
